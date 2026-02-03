4259c90dc348ff2a38db94331ba93384b254c921
Caleb O’Leary <226519682+caleboleary-me@users.noreply.github.com>
2026-02-03 10:57:14 +1000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-
+// App.tsx
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Menu from './components/Menu';
-import AIBarista from './components/AIBarista';
+// Remove this line, since the file is gone:
+// import AIBarista from './components/AIBarista';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -14,7 +15,7 @@ const App: React.FC = () => {
       <main>
         <Hero />
         <Menu />
-        <AIBarista />
+        {/* Remove <AIBarista /> as well */}
         <Contact />
       </main>
       <Footer />
